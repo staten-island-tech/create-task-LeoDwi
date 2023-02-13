82de25232aebbe3d80e27aa338b8545678e63ff9
LeoDwi <leod13579@gmail.com>
2023-02-13 09:55:44 -0500

    Update Dom.js

--- create-task/js/Dom.js
+++ create-task/js/Dom.js
@@ -2,4 +2,5 @@ export const DomSelectors = {
   randomButton: document.getElementById("random"),
   catButton: document.getElementById("cats"),
   dogButton: document.getElementById("dogs"),
+  parent: document.getElementById("parent"),
 };
