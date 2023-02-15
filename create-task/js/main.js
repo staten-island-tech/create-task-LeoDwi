9b609a2652838df7cd73ea5729b607b050c0ec44
LeoDwi <leod13579@gmail.com>
2023-02-15 10:15:56 -0500

    Update main.js

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -23,7 +23,7 @@ const catapis = {
   api: {
     link: "https://cat-fact.herokuapp.com",
     prefixes: {
-      facts: "/facts",
+      facts: "/facts/random?animal_type=cat&amount=2",
     },
   },
 };
@@ -31,7 +31,14 @@ const catapis = {
 async function getFact(apiLink) {
   try {
     let data = await fetch(apiLink);
+    let object = await data.json();
+    let facts = object.data;
+    console.log(object);
+    console.log(facts);
   } catch (error) {
     console.log(error);
   }
 }
+
+getFact("https://meowfacts.herokuapp.com/?count=1");
+getFact("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1");
